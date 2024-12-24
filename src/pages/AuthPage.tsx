c935968724c56786eeba06d55dae303632c714ef
taeyeonKim <tae8753@gmail.com>
2024-12-24 12:02:45 +0900

    ✨ feat: add viewModel status
    * add enum viewModelStatus
    * modity page with status

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -53,29 +53,6 @@ const AuthPage: React.FC = () => {
             }[status]}
         </div>
     );
-
-    if (!authorization.isAuthorized) return (
-        <div>
-            <h2>try Login</h2>
-            <form onSubmit={handleLogin}>
-                <label htmlFor="username">username</label>
-                <input type="text" id="username" value={username} placeholder="username"
-                       onChange={event => setUsername(event.target.value)}/>
-                <label htmlFor="password">password</label>
-                <input type="password" id="password" value={password} placeholder="password"
-                       onChange={event => setPassword(event.target.value)}/>
-                <button type={"submit"}>
-                    login
-                </button>
-            </form>
-        </div>
-    )
-    return (
-        <div>
-            <h2>Authorized</h2>
-            <button type={"button"} onClick={handleLogout}>logout</button>
-        </div>
-    )
 }
 
 export default AuthPage;
