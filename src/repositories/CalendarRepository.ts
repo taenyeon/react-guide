aa976ea0b3759deebe32307f65b2e6a248cffcf2
taeyeonKim <tae8753@gmail.com>
2025-01-10 18:06:58 +0900

    ✨ feat: modify CalendarRepository
    * modify Monthly Calendar logic

--- src/repositories/CalendarRepository.ts
+++ src/repositories/CalendarRepository.ts
@@ -4,22 +4,23 @@ import { Calendar } from '@typings/Calendar'
 
 const calendarRepository = {
   getMonthlyCalendar: (date: { year?: number; month?: number } = {}) => {
-    const { year, month } = date
-    const baseDate = dayjs()
+    const { year = 2024, month = 12 } = date
+    const now = dayjs()
 
     const calendar = new Calendar({
-      year: year ? year : baseDate.year(),
-      month: month ? month : baseDate.month() + 1,
+      year: year ? year : now.year(),
+      month: month ? month : now.month() + 1,
     })
 
     const dates: DateOfCalendar[] = []
     // 1일부터 세팅
-    let startDate = baseDate.date(1)
     // 특정 month가 있을 경우, 해당 month로 변경 -> dayjs month는 0이 1월
-    if (month) startDate = startDate.month(month - 1)
+    let startDate = dayjs(`${calendar.year}-${calendar.month}-1`)
+
     // 일요일부터 시작
-    startDate = startDate.subtract(startDate.day(), 'days')
+    console.log(startDate.day())
     console.log('startDate', startDate)
+    startDate = startDate.subtract(startDate.day(), 'days')
     // 월 단위 최대 갯수 = 35
     while (dates.length != 35) {
       const dateOfCalendar = new DateOfCalendar({
